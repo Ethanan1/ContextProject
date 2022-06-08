cb7475a192f10c4058049792ab9078bec1b9864f
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-06-07 20:49:43 -0700

    06 solved

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,21 @@
 // Your code here
+const Calculator = require('./02-calculator');
+
+class FancyCalculator extends Calculator {
+	setTotal(newTotal) {
+		this.total = newTotal;
+		return this.total;
+	}
+
+	modulo(num) {
+		this.total = this.total % num;
+		return this.total;
+	}
+
+	squared() {
+		return this.total *= this.total;
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
